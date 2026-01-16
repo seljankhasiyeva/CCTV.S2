5e1692523ed65e16a98638dfe4c4889fc49bf80e
Seljan Khasiyeva <seljankhasiyeva@gmail.com>
2026-01-16 14:59:39 +0400

    Add project files.

---
+++ Models/Base/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace CCTV.S2.Models.Base
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Models/TeamMember.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CCTV.S2.Models
+{
+    public class TeamMember:Base.BaseEntity
+    {
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Image { get; set; }
+    }
+}
